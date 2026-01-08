0fcd36d1af5ad4ae5df28d331fc6476af8854809
code-worker-me <abrian.work@gmail.com>
2026-01-08 14:32:41 +0700

    add databases

--- app/config/database.js
+++ app/config/database.js
@@ -1,1 +1,11 @@
-// TODO: Buat koneksi pool MySQL disini menggunakan Environment Variable (process.env)
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'arsip_digital'
+});
+
+module.exports = pool;
