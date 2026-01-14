ebfaaaf0077d90057e3202f82af6fdc65146d7e1
code-worker-me <abrian.work@gmail.com>
2026-01-14 21:28:32 +0700

    app.js aman

--- app/app.js
+++ app/app.js
@@ -1,16 +1,28 @@
 // TODO: Ini adalah titik masuk aplikasi, setup Express, Middleware, dan Server Listener disini
+// app/app.js
+require('dotenv').config();
 const express = require('express');
-
-const indexRoutes = require('./routes/index');
+const bodyParser = require("body-parser");
+const mainRoutes = require("./routes/index");
 
 const app = express();
-require('./routes/middleware')(app);
-
-// Route
-app.use('/', indexRoutes);
-
-// Run Server
 const PORT = process.env.APP_PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+
+/* =====================
+   MIDDLEWARE
+===================== */
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* =====================
+   Routes
+===================== */
+app.use('/', mainRoutes);
+
+/* =====================
+   SERVER LISTENER
+===================== */
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
